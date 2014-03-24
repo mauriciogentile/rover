4bf0375b2dc594690e6848acb4215f19828d4fcc
Mauricio Gentile <gentilemauricio@gmail.com>
2014-03-23 21:05:28 -0400

    Adding Terrain class.

---
+++ src/Rover.Test/TerrainTest.cs
@@ -0,0 +1,60 @@
+﻿using MarsRover;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Drawing;
+
+namespace MarsRover.Test
+{
+    [TestClass()]
+    public class TerrainTest
+    {
+        [TestMethod()]
+        public void should_add_obstacle()
+        {
+            var target = new Terrain();
+            var obstacle = new Point();
+            target.AddObstacle(obstacle);
+            Assert.IsTrue(target.HasObstacleAt(obstacle));
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void should_throw_error_when_adding_out_of_bound_obstacle()
+        {
+            var target = new Terrain { FarthestPoint = new Point(10, 10) };
+            var obstacle = new Point(11, 11);
+            target.AddObstacle(obstacle);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void should_throw_error_when_adding_out_of_bound_obstacle_1()
+        {
+            var target = new Terrain { FarthestPoint = new Point(10, 10) };
+            var obstacle = new Point(0, 11);
+            target.AddObstacle(obstacle);
+        }
+
+        [TestMethod()]
+        public void should_remove_existng_obstacle()
+        {
+            var target = new Terrain();
+            var obstacle = new Point();
+            target.AddObstacle(obstacle);
+            Assert.IsTrue(target.HasObstacleAt(obstacle));
+            target.RemoveObstacle(obstacle);
+            Assert.IsFalse(target.HasObstacleAt(obstacle));
+        }
+
+        [TestMethod()]
+        public void should_return_out_bands()
+        {
+            var target = new Terrain { FarthestPoint = new Point(10, 10) };
+
+            Assert.IsTrue(target.IsOutOfBounds(new Point(0, 11)));
+            Assert.IsTrue(target.IsOutOfBounds(new Point(-1, 11)));
+            Assert.IsTrue(target.IsOutOfBounds(new Point(-1, 10)));
+            Assert.IsTrue(target.IsOutOfBounds(new Point(11, 11)));
+        }
+    }
+}
---
+++ src/Rover/Terrain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace MarsRover
+{
+    public class Terrain
+    {
+        readonly List<Point> _obstacles;
+
+        public Terrain()
+        {
+            _obstacles = new List<Point>();
+        }
+
+        public Point FarthestPoint { get; set; }
+
+        public void AddObstacle(Point obstacle)
+        {
+            if (IsOutOfBounds(obstacle))
+            {
+                throw new InvalidOperationException("Obstacle out of range");
+            }
+            if (!_obstacles.Contains(obstacle))
+            {
+                _obstacles.Add(obstacle);
+            }
+        }
+
+        public void RemoveObstacle(Point obstacle)
+        {
+            _obstacles.Remove(obstacle);
+        }
+
+        public bool HasObstacleAt(Point position)
+        {
+            return _obstacles.Contains(position);
+        }
+
+        public bool IsOutOfBounds(Point position)
+        {
+            if (position.X < 0 || position.X > FarthestPoint.X || position.Y < 0 || position.Y > FarthestPoint.Y)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
